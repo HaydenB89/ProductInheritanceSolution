c8cbe0ca57283530d85445372953008e51435a0c
hayden brown <haydenbrowncinci@gmail.com>
2022-02-08 09:39:22 -0500

    Add project files.

---
+++ ProductInheritance/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInheritance {
+    public class Product {
+        //properties
+        private bool Flag { get; set; }
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+
+        //methods(actions)
+        public string ToPrint() {
+            return $"{GetType().ToString()} | {Code} | {Price:c}"; 
+
+        }
+        //Constructor (default would be no parameters (null or 0) if you dont create a Constructor)
+        public Product(string Code, decimal price) {
+            Flag = true;
+            this.Code = Code; // "this." notifies the system that the first 'Code' is the property (this. only works for properties and methods)
+            this.Price = price;
+        }
+    }
+}
---
+++ ProductInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductInheritance {
+    class Program {
+        static void Main(string[] args) {
+
+            Product p = new Product("Widget", 123.45m);
+            string message = p.ToPrint();
+            Console.WriteLine(message);
+        }
+    }
+}
