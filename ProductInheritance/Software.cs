f636cb4c0c47dcf1182b5052d50ded087d673f6f
hayden brown <haydenbrowncinci@gmail.com>
2022-02-08 13:48:17 -0500

    added software class

---
+++ ProductInheritance/Software.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInheritance {
+    public class Software:Product {
+        public bool Book { get; set; } = true;
+
+        public override string ToPrint() {
+            return $"{base.ToPrint()} | {(Book ? "Yes" : "No")}"; // turnery statement will produce yes/no instead of true/false
+        }
+
+        public Software(string code, decimal price, bool book)
+            :base(code, price) {
+            Book = book;
+        }
+        public Software() :base() {
+
+        }
+    }
+}
