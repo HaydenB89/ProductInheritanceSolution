984f3849ab6af600895f87f4e2b2c87c9653f4bf
hayden brown <haydenbrowncinci@gmail.com>
2022-02-08 11:53:03 -0500

    added List *note the foreach for including daughter classes to base

--- ProductInheritance/Program.cs
+++ ProductInheritance/Program.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductInheritance {
     class Program {
         static void Main(string[] args) {
 
-            Service s1 = new Service("BASIC", 100, "Basic", 3);
-            Console.WriteLine(s1.ToPrint());
+            List<Product> salesOrder = new List<Product>();
 
-            Widget w = new Widget("Widget", 543.21m, "Large", "MAX Red");
-            Console.WriteLine(w.ToPrint());
+            Product widget1 = new Widget("W1", 50, "Small", "Red");
+            Product widget2 = new Widget("W2", 100, "Medium", "White");
+            Product widget3 = new Widget("W3", 200, "Large", "Blue");
+            Product service = new Service("SVC", 500, "Premium", 5);
 
-            Product p = new Product("Echo", 123.45m);
-            string message = p.ToPrint();
-            Console.WriteLine(message);
+            salesOrder.Add(widget1);
+            salesOrder.Add(widget2);
+            salesOrder.Add(widget3);
+            salesOrder.Add(service);
+
+            foreach(Product p in salesOrder) {
+                if(p is Widget) {
+                    Widget w = p as Widget;                                         //you have to define p as sometimes being a Widget Class,
+                    Console.WriteLine($"{p.Code} {p.Price:c} {w.Size} {w.Color}");    //otherwise it only assumes Product class which does not 
+                }                                                                     //include all properties, just the base
+            }
+
+            //Service s1 = new Service("BASIC", 100, "Basic", 3);
+            //Console.WriteLine(s1.ToPrint());
+
+            //Widget w = new Widget("Widget", 543.21m, "Large", "MAX Red");
+            //Console.WriteLine(w.ToPrint());
+
+            //Product p = new Product("Echo", 123.45m);
+            //string message = p.ToPrint();
+            //Console.WriteLine(message);
         }
     }
 }
--- ProductInheritance/Service.cs
+++ ProductInheritance/Service.cs
@@ -9,6 +9,9 @@ namespace ProductInheritance {
         public string ServiceLevel { get; set; } = "Basic";
         public int NumberOfYears { get; set; } = 1;
 
+        public Service() :base() {
+
+        }
         public Service(string code, decimal price, string serviceLevel, int numberOfYears)
             :base(code, price) {
             ServiceLevel = serviceLevel;
